cde8ef7e4ad943e248294c165ef3f7ff338b917b
Arash Sadeghieh E <Arash@Arashs-MacBook-Pro.local>
2014-06-11 14:56:04 +1000

    added a test class

---
+++ gitTest/GITTest.h
@@ -0,0 +1,13 @@
+//
+//  GITTest.h
+//  gitTest
+//
+//  Created by Arash Sadeghieh E on 11/06/2014.
+//  Copyright (c) 2014 Arash Sadeghieh Eshtehadi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GITTest : NSObject
+
+@end
